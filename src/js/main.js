5eeeb77eae76e409e82f9fc6e9a5b9b155657689
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2024-01-04 21:21:26 +0100

    Add swiper library for carousel - not fully configured

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,36 @@
-console.log("Script loaded");
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+
+  // Navigation arrows
+  navigation: {
+    enabled: false,
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      navigation: {
+        enabled: true,
+      },
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+    renderProgressbar: function (progressbarFillClass) {
+      return '<span class="' + progressbarFillClass + '"></span>';
+    },
+  },
+});
+
+// Event lister fired when slide change
+swiper.on('slideChange', function () {
+  console.log(swiper.realIndex); // TODO chagne value in DOM
+});
+
+const numberOfSlides = swiper.slides.length;
